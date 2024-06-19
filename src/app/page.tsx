e4d11052380344f042285c5d86d5718d1f149ed2
kiranbaby14 <kiranbaby256@gmail.com>
2024-06-19 21:40:58 +0100

    add default mapbox package

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import DigitalTwin from '@/components/DigitalTwin'
 import Map from '@/components/Map'
+import Map2 from '@/components/Map2'
 
 
 export default function Home() {
@@ -9,10 +10,13 @@ export default function Home() {
     <>
       <Head>
         <link rel="stylesheet" href="cesium/Widgets/widgets.css" />
+        <link href="https://api.mapbox.com/mapbox-gl-js/v3.4.0/mapbox-gl.css" rel="stylesheet" />
+        <script src="https://api.mapbox.com/mapbox-gl-js/v3.4.0/mapbox-gl.js"></script>
       </Head>
       <div className='container'>
         <DigitalTwin className='left' />
-        <Map className='right' />
+        {/* <Map className='right' /> */}
+        <Map2 className='right' />
       </div>
 
     </>
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,34 +1,71 @@
 "use client"
 
 import { useRef, useState } from "react";
-import ReactMapGL, { ViewState, MapRef } from "react-map-gl";
+import ReactMapGL, { ViewState, MapRef, NavigationControl, Marker, CircleLayer, Source, Layer, FillLayer } from "react-map-gl";
 import 'mapbox-gl/dist/mapbox-gl.css';
+import Image from "next/image";
+import { FeatureCollection } from "geojson";
 
 
 interface Iprops {
 
 }
 
+const geojson: FeatureCollection = {
+  type: 'FeatureCollection',
+  features: [
+    { type: 'Feature', geometry: { type: 'Point', coordinates: [-122.4, 37.8] } }
+  ]
+};
+
+const parkLayer: FillLayer = {
+  id: 'landuse_park',
+  type: 'fill',
+  source: 'mapbox',
+  'source-layer': 'landuse',
+  filter: ['==', 'class', 'park'],
+  paint: {
+    'fill-color': '#4E3FC8'
+  }
+};
+
 const Map = ({ className }: { className: string }) => {
 
-  type excludedViesport = Omit<ViewState, 'bearing' | 'pitch' | 'padding'>;
+  type excludedViewState = Omit<ViewState, 'padding'>;
   const mapRef = useRef<MapRef | null>(null);
-  const [vieport, setViewport] = useState<excludedViesport>({
-    latitude: 43,
-    longitude: -79,
-    zoom: 10
+  const [viewport, setViewport] = useState<excludedViewState>({
+    latitude: 37.8,
+    longitude: -122.4,
+    // zoom: 10
+    zoom: 15.5,
+    pitch: 45,
+    bearing: -17.6,
+
   });
+
+
+  // console.log(mapRef);
 
   return (
     <div className={`${className}`}>
       <ReactMapGL
-        initialViewState={vieport}
+        {...viewport}
+        onMove={evt => setViewport(evt.viewState)}
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN}
         mapLib={import('mapbox-gl')}
-        mapStyle={"mapbox://styles/mapbox/streets-v9"}
+        mapStyle={"mapbox://styles/mapbox/streets-v12"}
         ref={mapRef}
-        minZoom={5}
-        maxZoom={15} />
+        >
+        <NavigationControl showCompass={false} />
+
+        <Marker draggable key='marker-example' latitude={37.89} longitude={-122.4} offset={[-15, -15]} color="red">
+          {/* <button>
+            <Image src={"vercel.svg"} alt="house" width={30} height={30} />
+          </button> */}
+        </Marker>
+        
+        {/* <Layer {...parkLayer} /> */}
+      </ReactMapGL>
     </div>
 
   )
